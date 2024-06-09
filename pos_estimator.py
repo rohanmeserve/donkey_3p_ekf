217dfc1428270d7647972995fc8f764e3028eb0c
rohanmeserve <rmserve@ucsd.edu>
2024-06-09 15:24:57 -0700

    last fix

--- pos_estimator.py
+++ pos_estimator.py
@@ -254,6 +254,8 @@ class GPS_IMU_EKF:
         
         self.last_pos_x = 0
         self.last_pos_y = 0
+        self.last_pred_x = 0
+        self.last_pred_y = 0
 
         # create file for analysis purposes
         self.file = open('ekf.csv', 'w')
